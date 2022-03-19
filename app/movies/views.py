7ced2cc8949c8821498845b5a57ba7d0db0315b9
David Venzke <digerian@protonmail.com>
2022-03-19 12:15:41 -0600

    setup APIView class

--- app/movies/views.py
+++ app/movies/views.py
@@ -1,8 +1,16 @@
-from django.shortcuts import render
-from django.http import JsonResponse
+from django.http import Http404
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
 
-# Create your views here.
+from .models import Movie
+from .serializers import MovieSerializer
 
-# def ping(request):
-#     data = {"ping": "pong!"}
-#     return JsonResponse(data)
+
+class MovieList(APIView):
+    def post(self, request, format=None):
+        serializer = MovieSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
