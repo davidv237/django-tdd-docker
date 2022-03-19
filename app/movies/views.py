dc1e57751315eff5289811c5d5fb8e44b4618773
David Venzke <digerian@protonmail.com>
2022-03-19 12:13:01 -0600

    tdd setup django rest framework with according tests

--- app/movies/views.py
+++ app/movies/views.py
@@ -3,6 +3,6 @@ from django.http import JsonResponse
 
 # Create your views here.
 
-def ping(request):
-    data = {"ping": "pong!"}
-    return JsonResponse(data)
+# def ping(request):
+#     data = {"ping": "pong!"}
+#     return JsonResponse(data)
